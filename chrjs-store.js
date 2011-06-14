c29c0e1c4bed150d84876305ed39a5dbf9093aac
Ben Gillies <bengillies@gmail.com>
2011-06-14 16:00:45 +0100

    Modified the Tiddlers object to remove prototypal inheritance
    Previously, I was using prototypal inheritance to inherit an Array,
    which I could then happily extend to create a new Array-like object
    without modifying Array.prototype itself.
    
    It turns out though that several functions of Arrays don't work when you
    do this:
    
    * The array.length property correctly keeps track when using push, etc
    * The automatic array extending (e.g. a = [1,2,3]; a[400] = 4; console.log(a.length)) doesn't work.
    * The array.concat function doesn't work
    * probably some more that I didn't test don't work either.
    
    It runs out that most stuff works fine, hence why I didn't come across
    this issue before. The code that highlighted this issue in Mustache.js,
    which does:
    
    	Object.prototype.toString.call(myArray) === '[object Array]'
    
    to check whether the object is an array (jQuery also does this).
    
    The Tiddlers object is now a function, that creates a new, blank, array
    and extends it directly with the helper functions, which seems to work,
    but doesn't seem quite as clean.

--- chrjs-store.js
+++ chrjs-store.js
@@ -16,158 +16,155 @@
 
 // the Tiddlers object is a list of tiddlers that you can operate on/filter. Get a list by calling the Store instance as a function (with optional filter)
 var Tiddlers = function(store, tiddlers) {
-	var self = this;
-	this.store = store;
+	var self = [];
+	self.store = store;
 	if (tiddlers) {
 		$.each(tiddlers, function(i, tiddler) {
 			self.push(tiddler);
 		});
 	}
-	return this;
-};
 
-Tiddlers.prototype = [];
+	// private functions
+	var contains = function(field, match) {
+		return (field && field.indexOf(match) !== -1) ? true : false;
+	};
 
-// private functions
-var contains = function(field, match) {
-	return (field && field.indexOf(match) !== -1) ? true : false;
-};
-
-// public functions
-$.extend(Tiddlers.prototype, {
-	tag: function(match) {
-		return this.map(function(tiddler) {
-			return contains(tiddler.tags, match) ? tiddler : null;
-		});
-	},
-	text: function(match) {
-		return this.map(function(tiddler) {
-			return contains(tiddler.text, match) ? tiddler : null;
-		});
-	},
-	title: function(match) {
-		return this.map(function(tiddler) {
-			return contains(tiddler.title, match) ? tiddler : null;
-		});
-	},
-	attr: function(name, match) {
-		var chkExists = (!match) ? true : false,
-			getValue = function(tiddler) {
-				return tiddler[name] || (tiddler.fields && tiddler.fields[name]);
-			};
-		return this.map(function(tiddler) {
-			if (chkExists) {
-				return (getValue(tiddler)) ? tiddler : null;
-			} else {
-				return contains(getValue(tiddler), match) ? tiddler : null;
-			}
-		});
-	},
-	bag: function(name) {
-		return this.map(function(tiddler) {
-			var bag = tiddler.bag && tiddler.bag.name;
-			return (bag === name) ? tiddler : null;
-		});
-	},
-	// the space the tiddler originates from (i.e. not just included in)
-	space: function(name) {
-		var regex = /(_public|_private|_archive)$/;
-		return this.map(function(tiddler) {
-			var bag = tiddler.bag && tiddler.bag.name;
-			return (bag.replace(regex, '') === name) ? tiddler : null;
-		});
-	},
-	// no arguments matches the default recipe
-	recipe: function(name) {
-		var matchCurrent = (name === undefined) ? true : false, recipe;
-		if (matchCurrent) {
-			recipe = this.store.recipe.name;
-		}
-		return this.map(function(tiddler) {
-			if (!matchCurrent) {
-				recipe = tiddler.recipe && tiddler.recipe.name;
-			}
-			return (recipe === name) ? tiddler : null;
-		});
-	},
-	// tiddlers that have been changed (i.e. not synced), lastSynced is optional and if present matches tiddlers that were synced before lastSynced
-	dirty: function(lastSynced) {
-		if (!lastSynced) {
-			return this.map(function(tiddler) {
-				return (tiddler.lastSync) ? null : tiddler;
+	// public functions
+	$.extend(self, {
+		tag: function(match) {
+			return self.map(function(tiddler) {
+				return contains(tiddler.tags, match) ? tiddler : null;
 			});
-		} else {
-			return this.map(function(tiddler) {
-				if (tiddler.lastSync) {
-					// return true if tiddler.lastSync is older than lastSynced
-					return (+tiddler.lastSync < +lastSynced) ? tiddler : null;
+		},
+		text: function(match) {
+			return self.map(function(tiddler) {
+				return contains(tiddler.text, match) ? tiddler : null;
+			});
+		},
+		title: function(match) {
+			return self.map(function(tiddler) {
+				return contains(tiddler.title, match) ? tiddler : null;
+			});
+		},
+		attr: function(name, match) {
+			var chkExists = (!match) ? true : false,
+				getValue = function(tiddler) {
+					return tiddler[name] || (tiddler.fields &&
+						tiddler.fields[name]);
+				};
+			return self.map(function(tiddler) {
+				if (chkExists) {
+					return (getValue(tiddler)) ? tiddler : null;
 				} else {
-					return tiddler;
+					return contains(getValue(tiddler), match) ? tiddler : null;
 				}
 			});
-		}
-	},
-	each: function(fn) {
-		var self = this;
-		$.each(self, function(i, tiddler) {
-			fn.apply(self, [tiddler, i]);
-		});
-		return self;
-	},
-	// returns a new instance of Tiddlers
-	map: function(fn) {
-		var self = this,
-			result = new Tiddlers(this.store);
-		$.each(self, function(i, tiddler) {
-			var mappedTiddler = fn.apply(self, [tiddler, i]);
-			if (mappedTiddler) {
-				result.push(mappedTiddler);
-			}
-		});
-		return result;
-	},
-	// pass in an initial value and a callback. Callback gets tiddler and current result, and returns new result
-	reduce: function(init, fn) {
-		var self = this, result = init;
-		$.each(self, function(i, tiddler) {
-			result = fn.apply(self, [tiddler, result]);
-		});
-		return result;
-	},
-	// bind fn to the current set of matched tiddlers.
-	bind: function(fn) {
-		var self = this,
-			bindFunc = function(tiddler) {
-				fn.apply(self, [tiddler]);
-			};
-		this.each(function(tiddler) {
-			self.store.bind('tiddler', tiddler.title, bindFunc);
-		});
-		return self;
-	},
-	// save tiddlers currently in list. Callback happens for each tiddler
-	save: function(callback) {
-		var self = this;
-		$.each(self, function(i, tiddler) {
-			self.store.saveTiddler(tiddler, callback);
-		});
-		return self;
-	},
-	// add one or more tiddlers to the current Tiddlers object and the attached store
-	add: function(tiddlers) {
-		var self = this;
-		if (tiddlers instanceof tiddlyweb.Tiddler) {
-			self.push(tiddlers);
-			self.store.addTiddler(tiddlers);
-		} else {
-			$.each(tiddlers, function(i, tiddler) {
-				self.push(tiddler);
-				self.store.addTiddler(tiddlers);
+		},
+		bag: function(name) {
+			return self.map(function(tiddler) {
+				var bag = tiddler.bag && tiddler.bag.name;
+				return (bag === name) ? tiddler : null;
 			});
+		},
+		// the space the tiddler originates from (i.e. not just included in)
+		space: function(name) {
+			var regex = /(_public|_private|_archive)$/;
+			return self.map(function(tiddler) {
+				var bag = tiddler.bag && tiddler.bag.name;
+				return (bag.replace(regex, '') === name) ? tiddler : null;
+			});
+		},
+		// no arguments matches the default recipe
+		recipe: function(name) {
+			var matchCurrent = (name === undefined) ? true : false, recipe;
+			if (matchCurrent) {
+				recipe = self.store.recipe.name;
+			}
+			return self.map(function(tiddler) {
+				if (!matchCurrent) {
+					recipe = tiddler.recipe && tiddler.recipe.name;
+				}
+				return (recipe === name) ? tiddler : null;
+			});
+		},
+		// tiddlers that have been changed (i.e. not synced), lastSynced is optional and if present matches tiddlers that were synced before lastSynced
+		dirty: function(lastSynced) {
+			if (!lastSynced) {
+				return self.map(function(tiddler) {
+					return (tiddler.lastSync) ? null : tiddler;
+				});
+			} else {
+				return self.map(function(tiddler) {
+					if (tiddler.lastSync) {
+						// return true if tiddler.lastSync is older than lastSynced
+						return (+tiddler.lastSync < +lastSynced) ? tiddler :
+							null;
+					} else {
+						return tiddler;
+					}
+				});
+			}
+		},
+		each: function(fn) {
+			$.each(self, function(i, tiddler) {
+				fn.apply(self, [tiddler, i]);
+			});
+			return self;
+		},
+		// returns a new instance of Tiddlers
+		map: function(fn) {
+			var result = Tiddlers(self.store);
+			$.each(self, function(i, tiddler) {
+				var mappedTiddler = fn.apply(self, [tiddler, i]);
+				if (mappedTiddler) {
+					result.push(mappedTiddler);
+				}
+			});
+			return result;
+		},
+		// pass in an initial value and a callback. Callback gets tiddler and current result, and returns new result
+		reduce: function(init, fn) {
+			var result = init;
+			$.each(self, function(i, tiddler) {
+				result = fn.apply(self, [tiddler, result]);
+			});
+			return result;
+		},
+		// bind fn to the current set of matched tiddlers.
+		bind: function(fn) {
+			var bindFunc = function(tiddler) {
+					fn.apply(self, [tiddler]);
+				};
+			self.each(function(tiddler) {
+				self.store.bind('tiddler', tiddler.title, bindFunc);
+			});
+			return self;
+		},
+		// save tiddlers currently in list. Callback happens for each tiddler
+		save: function(callback) {
+			$.each(self, function(i, tiddler) {
+				self.store.saveTiddler(tiddler, callback);
+			});
+			return self;
+		},
+		// add one or more tiddlers to the current Tiddlers object and the attached store
+		add: function(tiddlers) {
+			if (tiddlers instanceof tiddlyweb.Tiddler) {
+				self.push(tiddlers);
+				self.store.addTiddler(tiddlers);
+			} else {
+				$.each(tiddlers, function(i, tiddler) {
+					self.push(tiddler);
+					self.store.addTiddler(tiddlers);
+				});
+			}
+			return self;
 		}
-		return self;
-	}
-});
+	});
+
+	return self;
+};
+
 
 tiddlyweb.Store = function() {
 	// take in an optional filter and return a Tiddlers object with the tiddlers that match it
@@ -204,7 +201,7 @@ tiddlyweb.Store = function() {
 		},
 		store = {},
 		removeDeleted = function(container, tiddlers) {
-			var storeTids, newTiddlers = new Tiddlers(self, tiddlers),
+			var storeTids, newTiddlers = Tiddlers(self, tiddlers),
 				deleted = [];
 			newTiddlers = newTiddlers.map(function(tiddler) {
 				return tiddler.title;
@@ -267,7 +264,7 @@ tiddlyweb.Store = function() {
 
 	// public variables
 	self = function(name, match) {
-		var allTiddlers = new Tiddlers(this);
+		var allTiddlers = Tiddlers(self);
 
 		self.each(function(tiddler, title) {
 			allTiddlers.push(tiddler);
@@ -495,7 +492,7 @@ tiddlyweb.Store = function() {
 					name: 'RetrieveTiddlersError',
 					message: 'Error getting tiddler: ' + errMsg
 				});
-			}, (render) ? '?render=1' : '');
+			}, (render) ? 'render=1' : '');
 		} else {
 			callback(null);
 		}
