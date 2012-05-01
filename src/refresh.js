e707a9144f25e8e8c2cf580196f97d1dad80bc14
Ben Gillies <bengillies@gmail.com>
2012-05-01 14:45:16 +0100

    Added Websocket support for auto-refreshing tiddlers
    This purposefully fails silently if anything goes wrong during the
    process. It does this as it doesn't really matter if _all_ live updates
    happen in realtime or not. Plus deletes don't get notified either, so if
    you want an accurate update, just use the manual refresh command.
    
    The only thing worthy of note is that due to the nature of the
    tiddler_put/dispatcher support (i.e. no recipes), this code has an extra
    step where it checks the recipe cascade to find out if the tiddler is
    really in the recipe or not.
    
    Also, it currently doesn't work on searches. For that, chrjs-store needs
    to know about the tiddlyweb/tiddlyspace search syntax.

--- src/refresh.js
+++ src/refresh.js
@@ -1,8 +1,9 @@
 define(['utils'], function(utils) {
 
 return function(ev) {
-	var containers = {};
-	var replace = utils.replace(ev);
+	var containers = {},
+		replace = utils.replace(ev),
+		sockets = typeof io !== 'undefined';
 
 	var _removeDeleted = function(store, tiddlers) {
 		var keys = $.map(tiddlers, function(tid) { return tid.bag.name + tid.title; });
@@ -31,6 +32,58 @@ return function(ev) {
 		});
 	};
 
+	// only support websockets with bags and recipes
+	var _addSocket = function(sock, thing, store) {
+		var getTiddler = function(uri) {
+			var title = decodeURIComponent(uri.replace(/.*\//, ''));
+			return new tiddlyweb.Tiddler(title, thing);
+		};
+
+		var subscribeRecipe = function(recipe) {
+			$.each(recipe, function(i, bagFilterCombo) {
+				sock.emit('subscribe', 'bag/' + bagFilterCombo[0]);
+			});
+			sock.on('tiddler', function(uri) {
+				var tid = getTiddler(uri),
+					bagName = decodeURIComponent(uri.split('/')[2]);
+				tid.get(function(t) {
+					// if it's an update for a tiddler clobbered by the recipe
+					// bag stack, then ignore it
+					if (t.bag.name === bagName) {
+						replace(store, t);
+					}
+				}, function() {});
+			});
+		};
+
+		if (thing instanceof tiddlyweb.Recipe) {
+			if (thing.recipe.length === 0) {
+				thing.get(function(recipe) {
+					subscribeRecipe(recipe.recipe);
+				}, function() {}); // fail silently
+			} else {
+				subscribeRecipe(thing.recipe);
+			}
+		} else if (thing instanceof tiddlyweb.Bag) {
+			sock.emit('subscribe', 'bag/' + thing.name);
+			sock.on('tiddler', function(uri) {
+				var tid = getTiddler(uri);
+				tid.get(function(t) {
+					replace(store, t);
+				}, function() {}); // if error, then ignore the notification
+			});
+		}
+	};
+
+	// feature detect websockets and only use if they exist
+	var addSocket = (sockets) ? function(thing, store) {
+			var sock = io.connect(window.location.protocol + '//' +
+				window.location.hostname + ':8081');
+			sock.on('connect', function() {
+				_addSocket(sock, thing, store);
+			});
+		} : function() {};
+
 	return {
 		refresh: function(thing, callback) {
 			if (typeof thing !== 'function') {
@@ -46,6 +99,7 @@ return function(ev) {
 				tiddlers: (thing.tiddlers) ? thing.tiddlers() : thing,
 				store: store
 			};
+			addSocket(thing, store);
 		},
 		each: function(callback) {
 			$.each(containers, function(i, obj) {
