41aeb5c9502daa689bf46618d426ad46e886c70d
Ben Gillies <bengillies@gmail.com>
2012-05-01 14:51:52 +0100

    Released version 0.7.0
    New features include websocket support for bags and recipes, and a bug
    fix when using multiple stores.
    
    The websocket support means that, when available (i.e., when you include
    the socket.io library), refreshes will happen automatically without you
    having to do anything. Note that searches are currently unsupported in
    this respect.

--- chrjs-store.js
+++ chrjs-store.js
@@ -1349,8 +1349,9 @@ define('utils',[],function() {
 define('refresh',['utils'], function(utils) {
 
 return function(ev) {
-	var containers = {};
-	var replace = utils.replace(ev);
+	var containers = {},
+		replace = utils.replace(ev),
+		sockets = typeof io !== 'undefined';
 
 	var _removeDeleted = function(store, tiddlers) {
 		var keys = $.map(tiddlers, function(tid) { return tid.bag.name + tid.title; });
@@ -1379,6 +1380,58 @@ return function(ev) {
 		});
 	};
 
+	// only support websockets with bags and recipes
+	var _addSocket = function(sock, thing, store) {
+		var getTiddler = function(uri) {
+			var title = decodeURIComponent(uri.replace(/.*\//, ''));
+			return new tiddlyweb.Tiddler(title, thing);
+		};
+
+		var subscribeRecipe = function(recipe) {
+			$.each(recipe, function(i, bagFilterCombo) {
+				sock.emit('subscribe', 'bag/' + bagFilterCombo[0]);
+			});
+			sock.on('tiddler', function(uri) {
+				var tid = getTiddler(uri),
+					bagName = decodeURIComponent(uri.split('/')[2]);
+				tid.get(function(t) {
+					// if it's an update for a tiddler clobbered by the recipe
+					// bag stack, then ignore it
+					if (t.bag.name === bagName) {
+						replace(store, t);
+					}
+				}, function() {});
+			});
+		};
+
+		if (thing instanceof tiddlyweb.Recipe) {
+			if (thing.recipe.length === 0) {
+				thing.get(function(recipe) {
+					subscribeRecipe(recipe.recipe);
+				}, function() {}); // fail silently
+			} else {
+				subscribeRecipe(thing.recipe);
+			}
+		} else if (thing instanceof tiddlyweb.Bag) {
+			sock.emit('subscribe', 'bag/' + thing.name);
+			sock.on('tiddler', function(uri) {
+				var tid = getTiddler(uri);
+				tid.get(function(t) {
+					replace(store, t);
+				}, function() {}); // if error, then ignore the notification
+			});
+		}
+	};
+
+	// feature detect websockets and only use if they exist
+	var addSocket = (sockets) ? function(thing, store) {
+			var sock = io.connect(window.location.protocol + '//' +
+				window.location.hostname + ':8081');
+			sock.on('connect', function() {
+				_addSocket(sock, thing, store);
+			});
+		} : function() {};
+
 	return {
 		refresh: function(thing, callback) {
 			if (typeof thing !== 'function') {
@@ -1394,6 +1447,7 @@ return function(ev) {
 				tiddlers: (thing.tiddlers) ? thing.tiddlers() : thing,
 				store: store
 			};
+			addSocket(thing, store);
 		},
 		each: function(callback) {
 			$.each(containers, function(i, obj) {
@@ -1451,9 +1505,7 @@ return function(tiddlerCallback, getCached, defaultContainers) {
 
 	$.extend(self, {
 		refresh: function(callback) {
-			console.log('in refresh');
 			self.getDefaults(function(defaults) {
-				console.log('in callback from refresh');
 				// if no containers set, use the default one
 				var noContainers = true;
 				containers.each(function() { noContainers = false; });
@@ -1692,9 +1744,7 @@ return function(tiddlerCallback, getCached, defaultContainers) {
 	});
 
 	if (tiddlerCallback) {
-		console.log('about to tiddlerCallback');
 		self.getDefaults(function(c) {
-			console.log('in callback from tiddlerCallback');
 			containers.set(store, c.pullFrom);
 			self.refresh(tiddlerCallback);
 		});
